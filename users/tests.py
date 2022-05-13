6ad4ad7f57fd819285b34c52badae39f73841dac
김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>
2022-05-13 20:55:45 +0900

    feature:kakao login

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.unittest import mock,patch
+import json, bcrypt
 
-# Create your tests here.
+@patch('users.views.requests') #users.views에 등장하는 모든 requests의 값을 magicmock이라는 객체로 바꿔줌
+def test_success_kakaologin(self,mocked_requests):
+    client = Client()
+
+    class MockedResponse:
+        def json(self):
+            return {
+    "id": 2239940166,
+    "connected_at": "2022-05-12T16:57:36Z",
+    "properties": {
+        "nickname": "김영빈"
+    },
+    "kakao_account": {
+        "profile_nickname_needs_agreement": false,
+        "profile_image_needs_agreement": false,
+        "profile": {
+            "nickname": "김영빈",
+            "thumbnail_image_url": "http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg",
+            "profile_image_url": "http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg",
+            "is_default_image": true
+        },
+        "has_email": true,
+        "email_needs_agreement": false,
+        "is_email_valid": true,
+        "is_email_verified": true,
+        "email": "colock123@gmail.com"
+    }
+}
+    mocked_requests.get = MagicMock(return_value=MockedResponse())
+    #users.views.requests대신에 mocked_requests라는 게 들어가고,
+    #mocked_requests.get에 Mockedresponse가 들어간다
+    #mockedResponse는 결국 mockedResponse.json()와 같은 식으로 호출될 꺼니까, json함수를 넣어준 것임
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import json, jwt, requests
 
-# Create your views here.
+from django.http       import JsonResponse
+from django.views      import View
+from datetime          import datetime,timedelta
+from TerraBox.settings import ALGORITHM,SECRET_KEY
+
+from users.models import User
+
+def get_kakao_info(token):
+    kakao_url = "https://kapi.kakao.com/v2/user/me"
+    header ={
+        "Content-Type"  : "application/x-www-form-urlencoded",
+        "Authorization" : token,
+    }
+
+    return requests.get(kakao_url,headers=header).json()
+
+class KakaoLoginView(View):
+
+    def post(self,request):
+        try:
+            token = f"Bearer {request.headers.get('Authorization')}"
+
+            response          = get_kakao_info(token)
+            kakao_id          = response.get('id')
+            nickname          = response.get('properties').get('nickname')
+            profile_image_url = response.get('kakao_account').get('profile').get('thumbnail_image_url')
+            email             = response.get('kakao_account').get('email')
+            
+            user,created = User.objects.get_or_create(
+                kakao_id=kakao_id, 
+                defaults = {
+                    "nickname"         :nickname,
+                    "profile_image_url":profile_image_url,
+                    "email"            :email,
+                }
+            )
+
+            expiration = timedelta(seconds=3600)
+            token_expiration_time = datetime.utcnow() + expiration
+            
+            jwt_access_token = jwt.encode({'id':user.id,'exp':token_expiration_time},SECRET_KEY,algorithm=ALGORITHM)
+
+            return JsonResponse({'message':'success!',
+            'JWT_ACCESS_TOKEN':jwt_access_token,
+            "profile_image_url":user.profile_image_url,
+            "nickname":user.nickname,
+            "email":user.email},status=201)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)

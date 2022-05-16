bb63f3ce7a0bdcf8d7c344252fa7a9b4fc0d8876
김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>
2022-05-16 11:22:29 +0900

    modeling_fixed

--- reviews/models.py
+++ reviews/models.py
@@ -5,8 +5,6 @@ class MovieReview(TimeStampedModel):
     movie        = models.ForeignKey('movies.Movie',on_delete=models.CASCADE)
     user         = models.ForeignKey('users.USER',on_delete=models.CASCADE)
     content      = models.CharField(max_length=1000)
-    images_url   = models.CharField(max_length=1000,null=True)
-    storage_path = models.CharField(max_length=1000,null=True)
 
     class Meta:
         db_table = 'movie_reviews'
@@ -18,10 +16,20 @@ class UserReviewLike(TimeStampedModel):
     class Meta:
         db_table = 'user_review_likes'
 
-class UserReviewComment(TimeStampedModel):
-    review        = models.ForeignKey('MovieReview',on_delete=models.CASCADE)
-    user          = models.ForeignKey('users.USER',on_delete=models.CASCADE)
-    content       = models.CharField(max_length=1000)
+
+class MoviePost(TimeStampedModel):
+    movie        = models.ForeignKey('movies.Movie',on_delete=models.CASCADE)
+    user         = models.ForeignKey('users.USER',on_delete=models.CASCADE)
+    content      = models.CharField(max_length=1000)
+    images_url   = models.CharField(max_length=1000,null=True)
+    storage_path = models.CharField(max_length=1000,null=True)
 
     class Meta:
-        db_table = 'user_review_comments'
+        db_table = 'movie_posts'
+
+class UserPostLike(TimeStampedModel):
+    review        = models.ForeignKey('MoviePost',on_delete=models.CASCADE)
+    user          = models.ForeignKey('users.User',on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_post_likes'

805aa1d8e86e274eabac5aae61c806a3ce927811
humblebee <94585494+tbhumblestar@users.noreply.github.com>
2022-05-17 16:14:24 +0900

    ProductListView-GET (#4)
    Co-authored-by: 김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+from .models     import Movie,MovieImage
+
+class MovieListDetailTest(TestCase):
+    def setUp(self):
+
+        test_movies_list = [
+            Movie(
+            id             = 1,
+            name           = 'test_name1',
+            eng_name       = 'test_eng1',
+            description    = 'des1',
+            detail_text    = 'detail1',
+            age_grade      = 'test@test.com',
+            is_subtitle    = True,
+            screening_type = 2,
+            preview_url    = 'test@preview.com',
+            running_time   = 120,
+            ),
+            Movie(
+            id             = 2,
+            name           = 'test_name2',
+            eng_name       = 'test_eng2',
+            description    = 'des2',
+            detail_text    = 'detail2',
+            age_grade      = 'test@test.com',
+            is_subtitle    = True,
+            screening_type = 2,
+            preview_url    = 'test@preview.com',
+            running_time   = 220,
+            ),
+        ]
+
+        test_images_list = [
+            MovieImage(
+            movie_id=1,
+            stillcut_url='test_img1'
+            ),
+            MovieImage(
+            movie_id=2,
+            stillcut_url='test_img2'
+            ),
+        ]
+
+        Movie.objects.bulk_create(test_movies_list)
+        MovieImage.objects.bulk_create(test_images_list)
+
+    def tearDown(self):
+        Movie.objects.all().delete()
+    
+    def test_success_get_product_list(self):
+        client = Client()
+        response = client.get('/movies')
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.json(),{
+            "result":[
+                {
+                    'id'           : 1,
+                    'name'         : 'test_name1',
+                    'stillcut_url' : 'test_img1'
+                },
+                {
+                    'id'           : 2,
+                    'name'         : 'test_name2',
+                    'stillcut_url' : 'test_img2'
+                }
+            ],
+        })
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http      import JsonResponse
+from django.views     import View
+from .models          import Movie, MovieImage
 
-# Create your views here.
+class ProductListView(View):
+    """
+    사용처
+    -메인페이지의 캐러셀에 사용
+
+    반환
+    -영화 리스트를 반환
+    """
+
+
+    def get(self,requests):
+        
+        movies_list = Movie.objects.all().prefetch_related('movieimage_set')
+        result = [{
+            'id'          :movie.id,
+            'name'        :movie.name,
+            'stillcut_url':[movie.stillcut_url for movie in movie.movieimage_set.all()][0]
+        } for movie in movies_list]
+                
+        return JsonResponse({"result":result},status = 200)
+
+

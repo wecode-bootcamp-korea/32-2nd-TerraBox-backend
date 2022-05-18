86fd335b758cc64b48cefe1ce179c8942b1d1b1c
김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>
2022-05-18 17:04:02 +0900

    feat:MovieDetailView-GET

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
-from django.http    import JsonResponse
-from django.views   import View
-from movies.models  import Movie, MovieImage
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Count
+
+from movies.models    import Movie, MovieImage
+from reviews.models   import MoviePost
 
 class ProductListView(View):
     """
@@ -25,7 +28,8 @@ class ProductListView(View):
 
 class MovieDetailView(View):
     def get(self,request,movie_id):
-        movie = Movie.objects.get(id=movie_id)
+        movie = Movie.objects.prefetch_related('movieimage_set').get(id=movie_id)
+        movieposts = MoviePost.objects.filter(movie=movie).select_related('user').annotate(like_count = Count('userpostlike'))
         
         result = {
                 'id'            : movie.id,
@@ -34,6 +38,14 @@ class MovieDetailView(View):
                 'description'   : movie.description,
                 'stillcut_urls' : [image.stillcut_url for image in movie.movieimage_set.all()],
                 'preview_url'   : movie.preview_url,
+                'movieposts_count' : movie.moviepost_set.all().count(),
+                'movieposts'    : [{
+                    'movie_name' : movie.name,
+                    'user_name'  : moviepost.user.nickname,
+                    'content'    : moviepost.content,
+                    'images_url' : moviepost.images_url,
+                    
+                    }for moviepost in movieposts]
             } 
         
         return JsonResponse({'result':result}, status=200)

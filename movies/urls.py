f23036720d7893d6a70c6416a6b8eb3eef739921
김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>
2022-05-18 13:46:25 +0900

    fixed

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
-from django.urls   import path, include
+from django.urls   import path
 from .views        import ProductListView,MovieDetailView
 from reviews.views import MoviePostView
+from reviews.views import MoviePostDetailView
 
 urlpatterns = [
     path('',ProductListView.as_view()),
     path('/<int:movie_id>',MovieDetailView.as_view()),
     path('/<int:movie_id>/movieposts',MoviePostView.as_view()),
+    path('/<int:movie_id>/movieposts/<int:moviepost_id>',MoviePostDetailView.as_view())
 ]

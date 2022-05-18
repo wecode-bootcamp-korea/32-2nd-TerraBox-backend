5b56ad2c0b4166b5a452073def41809d8496e104
humblebee <94585494+tbhumblestar@users.noreply.github.com>
2022-05-18 11:24:59 +0900

    feature:kakao login (#8)
    Co-authored-by: 김영빈 <kimyoungbin@gim-yeongbin-ui-MacBookAir.local>

--- TerraBox/urls.py
+++ TerraBox/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('movies', include('movies.urls')),
+    path('users',include('users.urls')),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import KakaoLoginView
 
 urlpatterns = [
+    path('/login',KakaoLoginView.as_view())
 ]
